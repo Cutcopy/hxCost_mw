b7c79dcf5afbb242684cac620ea58eafe962d80c
Bryce Stradling <brycestradling@gmail.com>
2017-02-20 16:03:26 -0600

    updated copy, finished desktop view

--- js/main.js
+++ js/main.js
@@ -1,35 +1,161 @@
-// Init ScrollMagic
-// var ctrl = new ScrollMagic.Controller({
-//   globalSceneOptions: {
-//     triggerHook: 'onLeave'
-//   }
-// });
-//
-// // Create scene
-// $("section").each(function() {
-//
-//   var name = $(this).attr('id');
-//
-//   new ScrollMagic.Scene({
-//     triggerElement: this
-//   })
-//   .setPin(this)
-//   .addIndicators({
-//     colorStart: "rgba(255,255,255,0.5)",
-//     colorEnd: "rgba(255,255,255,0.5)",
-//     colorTrigger : "rgba(255,255,255,1)",
-//     name:name
-// 	})
-//   .loglevel(3)
-//   .addTo(ctrl);
-//
-// });
-//
-// // Get window height
-// var wh = window.innerHeight;
-//
-// new ScrollMagic.Scene({
-//   offset: wh*3
-// })
-// .setClassToggle("section.s1", "is-active")
-// .addTo(ctrl);
+$(document).ready(function() {
+    $('#fullpage').fullpage({
+        //Navigation
+        //menu: '#menu',
+        lockAnchors: false,
+        anchors:['hero', 'BeInControl', 'Know', 'signUp'],
+        navigation: true,
+        navigationPosition: 'left',
+        //navigationTooltips: ['firstSlide', 'secondSlide'],
+        showActiveTooltip: false,
+        slidesNavigation: false,
+        slidesNavPosition: 'bottom',
+
+        //Scrolling
+        css3: true,
+        scrollingSpeed: 700,
+        autoScrolling: true,
+        fitToSection: true,
+        fitToSectionDelay: 1000,
+        scrollBar: true,
+        easing: 'easeInOutCubic',
+        easingcss3: 'ease',
+        loopBottom: false,
+        loopTop: false,
+        loopHorizontal: true,
+        continuousVertical: false,
+        continuousHorizontal: false,
+        scrollHorizontally: false,
+        interlockedSlides: true,
+        dragAndMove: false,
+        offsetSections: false,
+        resetSliders: false,
+        fadingEffect: false,
+        //normalScrollElements: '.store_btns',
+        scrollOverflow: false,
+        scrollOverflowReset: false,
+        scrollOverflowOptions: null,
+        touchSensitivity: 15,
+        normalScrollElementTouchThreshold: 5,
+        bigSectionsDestination: null,
+
+        //Accessibility
+        keyboardScrolling: true,
+        animateAnchor: true,
+        recordHistory: true,
+
+        //Design
+        controlArrows: true,
+        verticalCentered: false,
+        //sectionsColor : ['#ccc', '#fff'],
+        padding: '3em',
+        fixedElements: '.store_btns',
+        responsiveWidth: 0,
+        responsiveHeight: 0,
+        responsiveSlides: false,
+
+        //Custom selectors
+        sectionSelector: '.section',
+        slideSelector: '.slide',
+
+        lazyLoading: true,
+
+        //events
+        onLeave: function(index, nextIndex, direction){},
+        afterLoad: function(anchorLink, index){},
+        afterRender: function(){},
+        afterResize: function(){},
+        afterResponsive: function(isResponsive){},
+        afterSlideLoad: function(anchorLink, index, slideAnchor, slideIndex){},
+        onSlideLeave: function(anchorLink, index, slideIndex, direction, nextSlideIndex){}
+    });
+  $('#play-link').click(function(){
+    $('.overlay').toggleClass('active');
+  });
+  $('.x-out').click(function(){
+    $('.overlay').removeClass('active');
+  });
+  $('.overlay').click(function(){
+    $(this).removeClass('active');
+  });
+});
+
+
+/*! fluidvids.js v2.4.1 | (c) 2014 @toddmotto | https://github.com/toddmotto/fluidvids */
+(function (root, factory) {
+  if (typeof define === 'function' && define.amd) {
+    define(factory);
+  } else if (typeof exports === 'object') {
+    module.exports = factory;
+  } else {
+    root.fluidvids = factory();
+  }
+})(this, function () {
+
+  'use strict';
+
+  var fluidvids = {
+    selector: ['iframe', 'object'],
+    players: ['www.youtube.com', 'player.vimeo.com']
+  };
+
+  var css = [
+    '.fluidvids {',
+      'width: 100%; max-width: 100%; position: relative;',
+    '}',
+    '.fluidvids-item {',
+      'position: absolute; top: 0px; left: 0px; width: 100%; height: 100%;',
+    '}'
+  ].join('');
+
+  var head = document.head || document.getElementsByTagName('head')[0];
+
+  function matches (src) {
+    return new RegExp('^(https?:)?\/\/(?:' + fluidvids.players.join('|') + ').*$', 'i').test(src);
+  }
+
+  function getRatio (height, width) {
+    return ((parseInt(height, 10) / parseInt(width, 10)) * 100) + '%';
+  }
+
+  function fluid (elem) {
+    if (!matches(elem.src) && !matches(elem.data) || !!elem.getAttribute('data-fluidvids')) return;
+    var wrap = document.createElement('div');
+    elem.parentNode.insertBefore(wrap, elem);
+    elem.className += (elem.className ? ' ' : '') + 'fluidvids-item';
+    elem.setAttribute('data-fluidvids', 'loaded');
+    wrap.className += 'fluidvids';
+    wrap.style.paddingTop = getRatio(elem.height, elem.width);
+    wrap.appendChild(elem);
+  }
+
+  function addStyles () {
+    var div = document.createElement('div');
+    div.innerHTML = '<p>x</p><style>' + css + '</style>';
+    head.appendChild(div.childNodes[1]);
+  }
+
+  fluidvids.render = function () {
+    var nodes = document.querySelectorAll(fluidvids.selector.join());
+    var i = nodes.length;
+    while (i--) {
+      fluid(nodes[i]);
+    }
+  };
+
+  fluidvids.init = function (obj) {
+    for (var key in obj) {
+      fluidvids[key] = obj[key];
+    }
+    fluidvids.render();
+    addStyles();
+  };
+
+  return fluidvids;
+
+});
+
+fluidvids.init({
+  selector: ['iframe', 'object'], // runs querySelectorAll()
+  players: ['www.youtube.com', 'player.vimeo.com'] // players to support
+});
